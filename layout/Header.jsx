a3899e5848731e6b14c5d906f487ea8b87f4ea0b
Alex Nweke <nwekealexchinedu@gmail.com>
2021-09-02 03:00:52 +0100

    started working on the header component

---
+++ layout/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Appbar, Title} from 'react-native-paper';
+import {View, Text} from 'react-native';
+
+const Header = () => {
+  const _goBack = () => console.log('Went back');
+
+  const _handleSearch = () => console.log('Searching');
+
+  const _handleMore = () => console.log('Shown more');
+
+  return (
+    <Appbar.Header>
+      <Appbar.BackAction onPress={_goBack} />
+      <Appbar.Content title="Title" subtitle="Subtitle" />
+      <Appbar.Action icon="magnify" onPress={_handleSearch} />
+      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+    </Appbar.Header>
+  );
+};
+
+export default Header;
