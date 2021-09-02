0bb20a7865d3f3c304968a7e36fa50a09c961d51
Alex Nweke <nwekealexchinedu@gmail.com>
2021-09-02 03:26:44 +0100

    started creating the search screen

--- layout/Header.jsx
+++ layout/Header.jsx
@@ -3,16 +3,10 @@ import { Appbar, Title } from 'react-native-paper';
 import { View, Text } from 'react-native';
 
 const Header = () => {
-  const _goBack = () => console.log('Went back');
-  const _handleSearch = () => console.log('Searching');
-  const _handleMore = () => console.log('Shown more');
 
   return (
-    <Appbar.Header>
-      <Appbar.BackAction onPress={_goBack} />
-      <Appbar.Content title="Title" subtitle="Subtitle" />
-      <Appbar.Action icon="magnify" onPress={_handleSearch} />
-      <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+    <Appbar.Header theme={{colors: {primary: "#00aaff"}}}>
+     <Title>Weather App</Title>
     </Appbar.Header>
   );
 };
---
+++ screens/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const Search = () => {
+    return(
+        <View>
+            <Text>
+                Hello from the search component
+            </Text>
+        </View>
+    );
+};
+
+export default Search;
